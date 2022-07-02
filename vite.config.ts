062e1cd34089ded8e9b71d909153c203cc2ead79
David <dadastudios382@gmail.com>
2022-07-02 07:04:26 -0500

    homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/typescript-products-app/",
   server: {
     proxy: {
       '/products': {
-        target: 'http://localhost:5000/',
+        target: 'https://products-server-1.herokuapp.com/',
         ws: true,
-        changeOrigin: true
+        changeOrigin: true,
+
       }
     }
   }
